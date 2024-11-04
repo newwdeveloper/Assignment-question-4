e156f2b7f30cf671f70d1d52ba27a66094328b0f
Akshay <akshaymangutkar951@gmail.com>
2024-11-04 15:30:03 +0530

    assignment question 4

--- src/index.js
+++ src/index.js
@@ -1,15 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const parent = React.createElement("div", { id: "container" }, [
+  React.createElement("input", {
+    type: "text",
+    id: "username",
+    name: "username",
+    placeHolder: "UserName",
+    class: "input",
+  }),
+  React.createElement("input", {
+    type: "password",
+    id: "password",
+    name: "password",
+    placeHolder: "Password",
+    class: "password",
+  }),
+  React.createElement("button", { type: "submit", id: "btn" }, "Submit"),
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<React.StrictMode>{parent}</React.StrictMode>);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
